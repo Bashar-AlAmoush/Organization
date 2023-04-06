97985d2ae6785955c48bc3cc469eef967eb8dbc2
Mufid Alnadi <mufidalnadi@gmail.com>
2023-04-06 11:27:21 +0300

    files

--- test.js
+++ test.js
@@ -5,11 +5,34 @@ console.log(JS);
 console.log(HTML);
 console.log(CSS);
 
+// function getExamTypeFromLocalStorage() {
+//     const userData = JSON.parse(localStorage.getItem("users"));
+//     const position = userData[0].position;
+  
+//     let examType;
+//     switch (position) {
+//       case "2":
+//         examType = HTML;
+//         break;
+//       case "3":
+//         examType = CSS;
+//         break;
+//       case "4":
+//         examType = JS;
+//         break;
+//       default:
+//         console.log("Invalid position value.");
+//     }
+  
+//     return examType;
+//   }
+  const exam = JS;
+  
 let currentQuestion = 0;
 // display the qustion from the array 
 
 function displayQuestion() {
-    let question = JS[currentQuestion];
+    let question = exam[currentQuestion];
     document.getElementById("question-text").innerHTML = question.question;
     document.getElementById("option1").nextSibling.textContent = question.optionA;
     document.getElementById("option2").nextSibling.textContent = question.optionB;
@@ -25,9 +48,10 @@ function displayQuestion() {
         });
     });
 
-    document.getElementById("question-count").textContent = `Question ${currentQuestion + 1} of ${JS.length}`;
+    document.getElementById("question-count").textContent = `Question ${currentQuestion + 1} of ${exam.length}`;
 }
- // Timer 
+displayQuestion();
+  // Timer 
  let quizTime = 8 * 60; // 8 minutes in seconds
  let intervalId = null;
  
@@ -53,28 +77,47 @@ function displayQuestion() {
    timerElement.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
  }
  
- let correctQuestions = [];
- let incorrectQuestions = [];
+;
  
- function selectAnswer() {
-     let selectedOption = document.querySelector('input[name="radio"]:checked');
-     if (!selectedOption) {
-         return;
-     }
-     let answer = selectedOption.id;
-     if (answer === JS[currentQuestion].correctOption) {
-         score++;
-         correctQuestions.push(JS[currentQuestion]);
-     } else {
-         incorrectQuestions.push(JS[currentQuestion]);
-     }
-     selectedOption.checked = false;
-     currentQuestion++;
-     if (currentQuestion === JS.length) {
-         endQuiz();
-     } else {
-         displayQuestion();
-     }
- }
- 
-  
+
+ function endQuiz() {
+    // (Stop the timer) stops the event of the recurrin
+    clearInterval(timer);
+    
+    // Save the score in local storage
+    localStorage.setItem("quizScore", score);
+    
+    // Save the list of correct and wrong answers in local storage
+    let correctQuestions = [];
+    let incorrectQuestions = [];
+    for (let i = 0; i < exam.length; i++) {
+      let answer = localStorage.getItem(`answer${i}`);
+      if (answer === exam[i].correctOption) {
+        correctQuestions.push(exam[i]);
+      } else {
+        incorrectQuestions.push(exam[i]);
+      }
+    }
+    localStorage.setItem("correctQuestions", JSON.stringify(correctQuestions));
+  localStorage.setItem("incorrectQuestions", JSON.stringify(incorrectQuestions));
+}
+
+function selectAnswer() {
+    let selectedOption = document.querySelector('input[name="radio"]:checked');
+    if (!selectedOption) {
+      return;
+    }
+    let answer = selectedOption.id;
+    localStorage.setItem(`answer${currentQuestion}`, answer);
+    if (answer === exam[currentQuestion].correctOption) {
+      score++;
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+    if (currentQuestion === exam.length) {
+      endQuiz();
+    } else {
+      displayQuestion();
+    }
+  }
+
